0b380de292eda7aed0f60f0497979207c1ed1839
Eike Kettner <eike.kettner@posteo.de>
2020-09-09 00:51:41 +0200

    Autoformat

--- modules/core/src/main/scala/yamusca/expand.scala
+++ modules/core/src/main/scala/yamusca/expand.scala
@@ -93,11 +93,10 @@ object expand {
               if (v.isEmpty) expandInner
               else Find.unit(())
             case ListValue(vs) =>
-              val list = vs.zipWithIndex.map({
-                case (v, i) =>
-                  expandInner
-                    .stacked(v.asContext)
-                    .stacked(Context.indexContext(i, vs.size))
+              val list = vs.zipWithIndex.map({ case (v, i) =>
+                expandInner
+                  .stacked(v.asContext)
+                  .stacked(Context.indexContext(i, vs.size))
               })
               list.foldLeft(Find.unit(()))(_ andThen _)
             case LambdaValue(f) =>
