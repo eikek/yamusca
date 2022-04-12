38c0d1bb486039030ae53635ba76db8416e99f3f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-12 22:38:40 +0200

    Update scala3-library, ... to 3.1.2 (#194)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Version {
     val scala213 = "2.13.8"
     val scala212 = "2.12.15"
-    val scala3 = "3.1.1"
+    val scala3 = "3.1.2"
 
     val munitVersion = "0.7.29"
     val munitCatsEffectVersion = "1.0.7"
