25ff298f466401512f95f9dec2257d56d2781ef4
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-06-13 22:45:08 +0000

    Update scala-library, scala-reflect to 2.12.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Version {
     val scala213 = "2.13.8"
-    val scala212 = "2.12.15"
+    val scala212 = "2.12.16"
     val scala3 = "3.1.2"
 
     val munitVersion = "0.7.29"
