2cbfd12793edf34e95d66db757eddc25cec893fa
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 16:27:01 +0000

    Update scala-library, scala-reflect to 2.12.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Version {
     val scala213 = "2.13.8"
-    val scala212 = "2.12.16"
+    val scala212 = "2.12.17"
     val scala3 = "3.2.0"
 
     val munitVersion = "0.7.29"
